c65d0cac19db931c6cb2d52f380aa633afae49d8
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-23 13:49:51 +0100

    adds explanation for indexOf solution:

--- DataStructures/indexOfToCheckArray.js
+++ DataStructures/indexOfToCheckArray.js
@@ -9,3 +9,5 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// Since arrays can be changed, or mutated, at any time, there's no guarantee about where a particular piece of data will be on a given array, or if that element even still exists. Luckily, JavaScript provides us with another built-in method, indexOf(), that allows us to quickly and easily check for the presence of an element on an array. indexOf() takes an element as a parameter, and when called, it returns the position, or index, of that element, or -1 if the element does not exist on the array.
