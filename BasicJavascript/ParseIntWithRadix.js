e82dfd5400d5be02402a316a937864dd7dff6465
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-23 12:34:36 +0100

    96% parses Int with radix 2, so converts binary number to integer

---
+++ BasicJavascript/ParseIntWithRadix.js
@@ -0,0 +1,9 @@
+function convertToInteger(str) {
+  var a = parseInt(str, 2);
+  return a;
+}
+
+convertToInteger("10011");
+
+// The radix variable says that "11" is in the binary system, or base 2. This example converts the string "11" to an integer 3.
+// Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
