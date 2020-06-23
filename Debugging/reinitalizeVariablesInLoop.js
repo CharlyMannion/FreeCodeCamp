1d7127b566261dc520f079d81de648440a02e7ab
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-23 13:21:25 +0100

    adds explanation

--- Debugging/reinitalizeVariablesInLoop.js
+++ Debugging/reinitalizeVariablesInLoop.js
@@ -1,3 +1,7 @@
+//The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+
+
+
 function zeroArray(m, n) {
   // Creates a 2-D array with m rows and n columns of zeroes
   let newArray = [];
