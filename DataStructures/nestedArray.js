7de9724985ecd84c7d22442771d984aac4ec1040
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-24 11:29:54 +0100

    75% of data structures - accessing properties in arrays

---
+++ DataStructures/nestedArray.js
@@ -0,0 +1,5 @@
+let myNestedArray = [ "one",
+  // Only change code below this line
+  ["two", ['deep', "three", ["deeper", "fourth", ["deepest", "fifth"]]]],
+  // Only change code above this line
+];
